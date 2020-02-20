921009e4fbebe4e15971acf97a4220111288070b
robsonalvz <robson.alves@dce.ufpb.br>
2020-02-20 15:17:28 -0300

    fix ddistance and duration values

--- src/modules/maps/controllers/RouteController.js
+++ src/modules/maps/controllers/RouteController.js
@@ -1,11 +1,13 @@
 import Routes from '../models/Routes';
 
 export const save = async (req, res) => {
-    const { origin, destination, waypoints } = req.body;
+    const { origin, destination, waypoints, duration, distance } = req.body;
     const data = await Routes.create({
         origin,
         destination,
         waypoints,
+        duration,
+        distance
       });
     return res.status(200).json(data);
 };
