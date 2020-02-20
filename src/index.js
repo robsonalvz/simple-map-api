80cdf2582be2a33511bd4ce124d289c22f7c54f7
robsonalvz <robson.alves@dce.ufpb.br>
2020-02-20 10:34:39 -0300

    :tada: add .env.example

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-//import * as Sentry from "@sentry/node";
 import mongoose from 'mongoose';
 import dotenv from "dotenv";
 import routes from "./routes";
@@ -18,19 +17,11 @@ mongoose.connect(
     { useNewUrlParser: true }
 );
 
-//Sentry.init({ dsn: process.env.SENTRY_DNS });
-
-
-//app.use(Sentry.Handlers.requestHandler());
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(routes);
-
-if (process.env.NODE_ENV === "production") {
-    app.use(Sentry.Handlers.errorHandler());
-}
 
 app.use(async (err, req, res, next) => {
     if (err instanceof validate.ValidationError) {
