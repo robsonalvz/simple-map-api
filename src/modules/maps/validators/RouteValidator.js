35b2a311c4ce359a3912a500710b7ffabff624f0
robsonalvz <robson.alves@dce.ufpb.br>
2020-02-20 13:36:53 -0300

    fix fields types

--- src/modules/maps/validators/RouteValidator.js
+++ src/modules/maps/validators/RouteValidator.js
@@ -1,17 +1,17 @@
 import Joi from "joi";
 const body = {
     origin: Joi.object().keys({
-        lat: Joi.string().required(),
-        long: Joi.string().required()
+        lat: Joi.number().required(),
+        long: Joi.number().required()
     }).required(),
     destination: Joi.object().keys({
-        lat: Joi.string().required(),
-        long: Joi.string().required()
+        lat: Joi.number().required(),
+        long: Joi.number().required()
     }).required(),
     waypoints: Joi.array().items(
         Joi.object().keys({
-            lat: Joi.string().required(),
-            long: Joi.string().required()
+            lat: Joi.number().required(),
+            long: Joi.number().required()
         })
     ).required()
 };
